5923eabb445b6917eb8ad5c0261ad48764b413a5
qkrgustn <sdh210212@sdh.hs.kr>
2022-11-04 09:47:21 +0900

    소싸움에서 시작, 끝 연출 적용

--- NovemberGameJam/Assets/Bark/Scripts/BullFight.cs
+++ NovemberGameJam/Assets/Bark/Scripts/BullFight.cs
@@ -1,17 +1,32 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BullFight : MonoBehaviour
 {
-    [SerializeField] private GameObject Cow1, Cow2, ResultWindow;
-    [SerializeField] private Text Result1, Result2;
-    private bool isGameStart = true;
+    [Header("ÀÎ°ÔÀÓ")]
+    [SerializeField] private GameObject Cow1, Cow2;
+    private bool isPlayGame;
 
     private int TouchNum;
 
+    [Header("UI")]
+    public Image fadePanel;
+    public ExplainScript explain;
+    public TextMeshProUGUI countDown;
+    public TextMeshProUGUI win;
+
+    bool isExplainShowEnd;
+
+    bool isEndGameOnce = false;
+
+    private void Start()
+    {
+        StartCoroutine(fadeIn());
+    }
     private void Update()
     {
         Cow1.transform.DOMove(new Vector2(-4f + (TouchNum / 4f), 0), 0.1f);
@@ -19,15 +34,75 @@ public class BullFight : MonoBehaviour
         GameEnd();
         InputTouch();
     }
+    IEnumerator fadeIn()
+    {
+        countDown.gameObject.SetActive(false);
+        fadePanel.gameObject.SetActive(true);
+        win.gameObject.SetActive(false);
+        var fadePanelColor = fadePanel.color;
+        fadePanelColor.a = 1;
+        fadePanel.color = fadePanelColor;
+
+        for (float i = 1; i > 0.8f; i -= Time.deltaTime / 3)
+        {
+            fadePanelColor.a = i;
+            fadePanel.color = fadePanelColor;
+            yield return new WaitForEndOfFrame();
+        }
+
+        explain.showExplain(new Vector2(0, 1200), new Vector2(0, 0), () =>
+        {
+            isExplainShowEnd = true;
+        });
+
+        while (!isExplainShowEnd) yield return new WaitForEndOfFrame();
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        for (float t = fadePanelColor.a; t > 0; t -= Time.deltaTime / 2)
+        {
+            fadePanelColor.a = t;
+            fadePanel.color = fadePanelColor;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(1);
+        countDown.gameObject.SetActive(true);
+        countDown.text = "3";
+        yield return new WaitForSeconds(1);
+        countDown.text = "2";
+        yield return new WaitForSeconds(1);
+        countDown.text = "1";
+        yield return new WaitForSeconds(1);
+        countDown.text = "½ÃÀÛ!";
+        yield return new WaitForSeconds(1);
+
+        isPlayGame = true;
+        countDown.gameObject.SetActive(false);
+    }
+    IEnumerator EndGame()
+    {
+        win.gameObject.SetActive(true);
+        win.text = TouchNum >= 0 ? "±è ´ë°¨ ¿ì½Â!" : "Á¤ ´ë°¨ ¿ì½Â!";
+        yield return new WaitForSeconds(1);
+        for (float t = fadePanel.color.a; t < 1; t += Time.deltaTime / 2)
+        {
+            var fadePanelColor = fadePanel.color;
+            fadePanelColor.a = t;
+            fadePanel.color = fadePanelColor;
+            yield return new WaitForEndOfFrame();
+        }
+        YutGameManager.manager.isPlayMiniGame = false;
+    }
     void InputTouch()
     {
-        if (isGameStart)
+        if (isPlayGame)
         {
-            if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
             {
                 TouchNum++;
             }
-            if(Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L))
+            if (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L))
             {
                 TouchNum--;
             }
@@ -37,19 +112,22 @@ public class BullFight : MonoBehaviour
     {
         if (TouchNum >= 42)
         {
-            isGameStart = false;
-            ResultWindow.SetActive(true);
-            ResultWindow.gameObject.transform.DOScale(new Vector3(1, 1, 1), 0.7f);
-            Result1.text = "Player1";
-            Result2.text = "Player2";
+            isPlayGame = false;
+            if (!isEndGameOnce)
+            {
+
+                isEndGameOnce = true;
+                StartCoroutine(EndGame());
+            }
         }
         else if (TouchNum <= -42)
         {
-            isGameStart = false;
-            ResultWindow.SetActive(true);
-            ResultWindow.gameObject.transform.DOScale(new Vector3(1, 1, 1), 0.7f);
-            Result1.text = "Player2";
-            Result2.text = "Player1";
+            isPlayGame = false;
+            if (!isEndGameOnce)
+            {
+                isEndGameOnce = true;
+                StartCoroutine(EndGame());
+            }
         }
     }
 }
